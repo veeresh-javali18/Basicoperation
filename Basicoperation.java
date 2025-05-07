26f6149dd946e33edc0a6e327d2642b3a941e3d3
veeresh-javali18 <veereshjavali038@gmail.com>
2025-05-07 14:57:42 +0530

    Basic operation

---
+++ Basicoperation.java
@@ -0,0 +1,16 @@
+
+
+public class Basicoperation {
+    public static void main(String[] args) {
+        int a=10;
+        int b=10;
+        int sum=a+b;
+        int sub=a-b;
+        int multiplication=a*b;
+        System.out.println("Addition :"+sum);
+        System.out.println("substraction"+sub);
+        System.out.println("multiplication:"+multiplication);
+
+    }
+    
+}
